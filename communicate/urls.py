929f5f4a08f099dfd17033bf57ab461979ac6d9b
eiji.iriyama <eiji.iriyama@zmp.co.jp>
2020-12-30 13:52:26 +0900

    update url

--- communicate/urls.py
+++ communicate/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('report/', views.report_user, name='report_user'),
+    path('report/user', views.report_user, name='report_user'),
     # Message
-    path('', views.add_message, name='add_message'),
+    path('message/send', views.add_message, name='add_message'),
     # Conversation
     path('conversation/<int:conversation_id>/view/', views.view_conversation, name='conversation_view'),
     path('conversation/<int:conversation_id>/delete/', views.delete_conversation, name='conversation_delete'),
