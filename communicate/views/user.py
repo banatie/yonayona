1c9c86620afc4df3d09bfb6f92ed03c60c60f28c
eiji.iriyama <eiji.iriyama@zmp.co.jp>
2020-12-30 17:10:52 +0900

    implement signup

--- communicate/views/user.py
+++ communicate/views/user.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
@@ -7,38 +9,58 @@ def index(request):
     #  render to index.html
     # Logged In
     #  render to user.html
-    context = {}
-    if True:
-        # Logged in
-        return render(request, 'communicate/index.html', context)
-    else:
-        return render(request, 'communicate/user.html', context)
+    return render(request, 'communicate/user.html', {})
+
+
 def about(request):
     context = {}
     return render(request, 'communicate/about.html', context)
-def signup(request):
+
+
+def user_signup(request):
+    if request.user.is_authenticated:
+        # Already logged in
+        context = {'error_message' : 'User already logged in'}
+        return render(request, 'communicate/index.html', context)
+
     # GET
-    #  render to signup.html
+    if request.method == 'GET':
+        return render(request, 'communicate/signup.html', {})
     # POST
-    # valid email
-    # valid password
-    #  render to user.html
-    context = {}
-    if True:
-        # Validated
-        return render(request, 'communicate/user.html', context)
-    else:
-        return render(request, 'communicate/signup.html', context)
-def login(request):
+    if request.method == 'POST':
+        try:
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            email = request.POST['email']
+            password = request.POST['password']
+        except KeyError:
+            context = {'error_message' : 'Some form item is not found'}
+            return render(request, 'communicate/signup.html', context)
+        # valid password
+        user = None
+        try:
+            user = User.objects.get(username=email)
+        except User.DoesNotExist:
+            pass
+
+        if user is not None:
+            context = {'error_message' : 'The Email is already taken'}
+            return render(request, 'communicate/signup.html', context)
+        else:
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=email, email=email, password=password)
+            login(request, user)
+            return render(request, 'todolist/index.html', {})
+
+def user_login(request):
     # POST
     # not logged in
     # user exists
-    #  render to user.html
+    #  render to index.html
     # GET
     #  render to index view
     context = {}
-    return render(request, 'communicate/user.html', context)
-def logout(request):
+    return render(request, 'communicate/index.html', context)
+def user_logout(request):
     # logged in
     # logout
     #  render to index.html
@@ -47,4 +69,4 @@ def logout(request):
 def report_user(request):
     # Add Welcome Message
     context = {}
-    return render(request, 'communicate/user.html', context)
+    return render(request, 'communicate/index.html', context)
