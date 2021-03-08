33efd89d5aa100fe0faa6bd6d3e24cdd25691d1f
iriyama <eiji.iriyama.japan@gmail.com>
2021-03-09 02:50:22 +0900

    add crontab for end all conversations

--- communicate/cron.py
+++ communicate/cron.py
@@ -1,9 +1,21 @@
 from .utils import availability_utils
+from .models import Conversation
+from .websocket import server_utils
 
 
 def end_all_conversation():
     if availability_utils.is_available():
         return
 
-    # End all conversation
-    print('ended all conversations')
+    try:
+        # End all conversation
+        conversations = Conversation.objects.filter(is_active=True)
+    except Conversation.DoesNotExist:
+        pass
+
+    for conversation in conversations:
+        conversation.is_active = False
+        conversation.save()
+
+        # send end command
+        server_utils.send_command(conversation_id=conversation.id, command="end")
