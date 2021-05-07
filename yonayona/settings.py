b85faf7e29c91e1e8f988f1ea28619bc19328a61
iriyama <eiji.iriyama.japan@gmail.com>
2021-05-08 02:03:26 +0900

    remove redis cache

--- yonayona/settings.py
+++ yonayona/settings.py
@@ -140,12 +140,6 @@ CHANNEL_LAYERS = {
         },
     },
 }
-CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.RedisCache',
-        'LOCATION': os.environ.get('REDIS_URL'),
-    }
-}
 
 CRONJOBS = [
     ('0 */1 * * *', 'communicate.cron.end_all_conversation')
