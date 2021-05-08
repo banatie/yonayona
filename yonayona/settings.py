f3a096079f804ca6f4209f27db0144d2df01c44c
iriyama <eiji.iriyama.japan@gmail.com>
2021-05-08 21:31:14 +0900

    add redis server name

--- yonayona/settings.py
+++ yonayona/settings.py
@@ -136,7 +136,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL')],
+            "hosts": [('redis-concentric-63909', 6379)],
         },
     },
 }
