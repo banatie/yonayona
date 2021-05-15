eb16445bbe1538bb58b8acbc4a8015b54466ffd7
iriyama <eiji.iriyama.japan@gmail.com>
2021-05-15 14:18:33 +0900

    hosts should be a list of tuple

--- yonayona/settings.py
+++ yonayona/settings.py
@@ -136,7 +136,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.environ.get('REDIS_URL', 'localhost'), 6379],
+            "hosts": [(os.environ.get('REDIS_URL', 'localhost'), 6379)],
         },
     },
 }
